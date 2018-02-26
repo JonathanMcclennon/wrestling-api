ebd8d4366f59b51315793d2eaae9adba6560561b
Jon  McClennon <jon.mcclennon@gmail.com>
2018-02-26 21:47:17 +0000

    Allows args to be passed through

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const sampleSize = require('lodash/fp/sampleSize');
 console.info(sampleSize)
 const wreslters = require('./wrestlers.json');
+
+const numberOptions = [8, 16, 32];
 const create = (number = 32, type) => {
-    const _wreslters = [...wreslters.singles];
-    console.info()
+    const data = type === 'tag' ? wreslters.tags : wreslters.singles;
+    number = numberOptions.indexOf(Number(number)) === -1 ? numberOptions[2] : numberOptions[numberOptions.indexOf(Number(number))];
+    const _wreslters = [...data];
+    console.info(Number(number))
     return sampleSize(number, _wreslters);
 }
 
 
 app.use(cors())
 app.get('/api/v1/tournament', (req, res) => {
-    return res.json({ data: create() })
+    console.info(req.query)
+    return res.json({ data: create(req.query.amount, req.query.type) })
 });
 
 app.listen(3001, () => console.log('Example app listening on port 3001!'))
